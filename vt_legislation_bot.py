dec1c3cb6e140b3254142b49f68cf66a2711efb0
Jonathan Cormier <jjcf89@gmail.com>
2015-02-11 21:51:06 -0500

    Add BeautifulSoup Test

--- vt_legislation_bot.py
+++ vt_legislation_bot.py
@@ -2,6 +2,7 @@
 
 import argparse
 import urllib
+from bs4 import BeautifulSoup
 
 #TODO Parse arguments
 URL="http://legislature.vermont.gov/bill/status/2016/H.159"
@@ -12,4 +13,8 @@ def fetch_url(url):
 	f = opener.open(url)
 	return f.read()
 
-print fetch_url(URL)
+page = fetch_url(URL)
+
+soup = BeautifulSoup(page)
+
+print soup.select("h4 .charge")
